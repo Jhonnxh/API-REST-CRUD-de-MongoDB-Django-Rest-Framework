ae0b8c8bbc36e6f5504104b0477b5f2b5ce9746a
Jhonnxh <jhonny.hernandez1807@gmail.com>
2023-03-11 00:56:26 -0600

    Primer avance

---
+++ tutorials/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializer
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['GET'])
+def tutorial_list_published(request):
+    tutorials = Tutorial.objects.filter(published=True)
+        
+    if request.method == 'GET': 
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+    # GET all published tutorials
